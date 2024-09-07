b307c8dadea78c2cd57c0e15356663e8ee202ffd
Daryna Vlasiuk <s26878@pjwstk.edu.pl>
2024-09-08 01:26:36 +0200

    Updated database, fixed book covers display

--- src/main/resources/data/data.sql
+++ src/main/resources/data/data.sql
@@ -18,7 +18,11 @@ INSERT INTO Author (FIRST_NAME, LAST_NAME, DATE_OF_BIRTH) VALUES ('Mary', 'Shell
 INSERT INTO Author (FIRST_NAME, LAST_NAME, DATE_OF_BIRTH) VALUES ('Jules', 'Verne', '1828-02-08');
 INSERT INTO Author (FIRST_NAME, LAST_NAME, DATE_OF_BIRTH) VALUES ('George', 'R.R. Martin', '1948-09-20');
 INSERT INTO Author (FIRST_NAME, LAST_NAME, DATE_OF_BIRTH) VALUES ('C.S.', 'Lewis', '1898-11-29');
-INSERT INTO Author (FIRST_NAME, LAST_NAME, DATE_OF_BIRTH) VALUES ('Edgar Allan', 'Poe', '1809-01-19');
+INSERT INTO Author (FIRST_NAME, LAST_NAME, DATE_OF_BIRTH) VALUES ('Edgar Allan', 'Poe', '1809-01-19'); --20
+INSERT INTO Author (FIRST_NAME, LAST_NAME, DATE_OF_BIRTH) VALUES ('Lewis', 'Carroll', '1832-01-27');
+INSERT INTO Author (FIRST_NAME, LAST_NAME, DATE_OF_BIRTH) VALUES ('Nicolas', 'Sparks', '1965-12-31');
+INSERT INTO Author (FIRST_NAME, LAST_NAME, DATE_OF_BIRTH) VALUES ('Charlotte', 'Brontë', '1816-04-21'); --23
+
 
 -- Insert data into Publisher table
 INSERT INTO Publisher (Name) VALUES ('Penguin Books');
@@ -41,80 +45,112 @@ INSERT INTO Publisher (Name) VALUES ('Cengage');
 INSERT INTO Publisher (Name) VALUES ('Brill');
 INSERT INTO Publisher (Name) VALUES ('SAGE');
 INSERT INTO Publisher (Name) VALUES ('Taylor & Francis');
+INSERT INTO Publisher (Name) VALUES ('Brimax Publishing'); --21
+INSERT INTO Publisher (Name) VALUES ('Warner Books');
 
 -- Insert data into Genre table
 INSERT INTO Genre (Name) VALUES ('Dystopian');
 INSERT INTO Genre (Name) VALUES ('Romance');
-INSERT INTO Genre (Name) VALUES ('Fantasy');
+INSERT INTO Genre (Name) VALUES ('Fantasy'); --3
 INSERT INTO Genre (Name) VALUES ('Science Fiction');
 INSERT INTO Genre (Name) VALUES ('Mystery');
-INSERT INTO Genre (Name) VALUES ('Thriller');
+INSERT INTO Genre (Name) VALUES ('Thriller'); --6
 INSERT INTO Genre (Name) VALUES ('Horror');
 INSERT INTO Genre (Name) VALUES ('Historical Fiction');
 INSERT INTO Genre (Name) VALUES ('Adventure');
-INSERT INTO Genre (Name) VALUES ('Biography');
+INSERT INTO Genre (Name) VALUES ('Biography'); --10
 INSERT INTO Genre (Name) VALUES ('Self-Help');
 INSERT INTO Genre (Name) VALUES ('Non-Fiction');
-INSERT INTO Genre (Name) VALUES ('Poetry');
+INSERT INTO Genre (Name) VALUES ('Poetry'); --13
 INSERT INTO Genre (Name) VALUES ('Drama');
 INSERT INTO Genre (Name) VALUES ('Classics');
-INSERT INTO Genre (Name) VALUES ('Comics');
+INSERT INTO Genre (Name) VALUES ('Comics'); --16
 INSERT INTO Genre (Name) VALUES ('Graphic Novels');
 INSERT INTO Genre (Name) VALUES ('Childrens');
 INSERT INTO Genre (Name) VALUES ('Young Adult');
-INSERT INTO Genre (Name) VALUES ('Philosophy');
+INSERT INTO Genre (Name) VALUES ('Philosophy'); --20
 
 -- Insert data into Book table
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('1984', 'https://images-na.ssl-images-amazon.com/images/I/41GDv5AMeNL._SX331_BO1,204,203,200_.jpg', 4.7, '1949-06-08', 1, 1);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Pride and Prejudice', 'https://images-na.ssl-images-amazon.com/images/I/81A-mvlo+QL.jpg', 4.6, '1813-01-28', 2, 2);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Harry Potter and the Sorcerers Stone', 'https://images-na.ssl-images-amazon.com/images/I/81YOuOGFCJL.jpg', 4.9, '1997-06-26', 3, 3);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Adventures of Tom Sawyer', 'https://images-na.ssl-images-amazon.com/images/I/91HHqVTAJQL.jpg', 4.5, '1876-12-17', 4, 4);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Great Gatsby', 'https://images-na.ssl-images-amazon.com/images/I/81af+MCATTL.jpg', 4.4, '1925-04-10', 5, 5);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Old Man and the Sea', 'https://images-na.ssl-images-amazon.com/images/I/81t2CVWEsUL.jpg', 4.3, '1952-09-01', 6, 6);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('A Tale of Two Cities', 'https://images-na.ssl-images-amazon.com/images/I/91UoQHRbMPL.jpg', 4.6, '1859-04-30', 7, 7);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('War and Peace', 'https://images-na.ssl-images-amazon.com/images/I/91iG4jbwRHL.jpg', 4.8, '1869-01-01', 8, 8);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Moby-Dick', 'https://images-na.ssl-images-amazon.com/images/I/81Pr+bp2gLL.jpg', 4.2, '1851-10-18', 9, 9);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The War of the Worlds', 'https://images-na.ssl-images-amazon.com/images/I/81w1NPNBbAL.jpg', 4.5, '1898-04-01', 10, 10);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Foundation', 'https://images-na.ssl-images-amazon.com/images/I/91yXOZIxtlL.jpg', 4.4, '1951-05-03', 11, 11);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Hobbit', 'https://images-na.ssl-images-amazon.com/images/I/91b0C2YNSrL.jpg', 4.8, '1937-09-21', 12, 12);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Murder on the Orient Express', 'https://images-na.ssl-images-amazon.com/images/I/71kxa1-0mfL.jpg', 4.6, '1934-01-01', 13, 13);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Hound of the Baskervilles', 'https://images-na.ssl-images-amazon.com/images/I/81e8y-9n1ML.jpg', 4.7, '1902-04-01', 14, 14);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Brave New World', 'https://images-na.ssl-images-amazon.com/images/I/81aYyyhllzL.jpg', 4.3, '1932-08-18', 15, 15);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Frankenstein', 'https://images-na.ssl-images-amazon.com/images/I/81WcnNQ-TBL.jpg', 4.4, '1818-01-01', 16, 16);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Twenty Thousand Leagues Under the Sea', 'https://images-na.ssl-images-amazon.com/images/I/91L4ZNYE1rL.jpg', 4.5, '1870-06-20', 17, 17);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('A Game of Thrones', 'https://images-na.ssl-images-amazon.com/images/I/91b0C2YNSrL.jpg', 4.9, '1996-08-06', 18, 18);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Lion, the Witch and the Wardrobe', 'https://images-na.ssl-images-amazon.com/images/I/81YoUOjEhLL.jpg', 4.8, '1950-10-16', 19, 19);
-INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Raven', 'https://images-na.ssl-images-amazon.com/images/I/71msq8D8ZSL.jpg', 4.3, '1845-01-29', 20, 20);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('1984', '9781782124207', 4.7, '1949-06-08', 1, 1);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Pride and Prejudice', '9787511024831', 4.6, '1813-01-28', 2, 2);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Harry Potter and the Sorcerers Stone', '9780590353427', 4.9, '1997-06-26', 3, 3);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Adventures of Tom Sawyer', '9780600553106', 4.5, '1876-12-17', 4, 4);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Great Gatsby', '9781915582157', 4.4, '1925-04-10', 5, 5);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Old Man and the Sea', '9781791335670', 4.3, '1952-09-01', 6, 6);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('A Tale of Two Cities', '9780195854527', 4.6, '1859-04-30', 7, 7);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('War and Peace', '9781125536346', 4.8, '1869-01-01', 8, 8);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Moby-Dick', '9780198853695', 4.2, '1851-10-18', 9, 9);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The War of the Worlds', '9780141026008', 4.5, '1898-04-01', 10, 10);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Foundation', '9782070360536', 4.4, '1951-05-03', 11, 11);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Hobbit', '9780547928227', 4.8, '1937-09-21', 12, 12);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Murder on the Orient Express', '9780008268879', 4.6, '1934-01-01', 13, 13);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Hound of the Baskervilles', '9781537349145', 4.7, '1902-04-01', 14, 14);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Brave New World', '9781785996207', 4.3, '1932-08-18', 15, 15);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Frankenstein', '9781546690061', 4.4, '1818-01-01', 16, 16);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('Twenty Thousand Leagues Under the Sea', '9781502969262', 4.5, '1870-06-20', 17, 17);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('A Game of Thrones', '9780007448036', 4.9, '1996-08-06', 18, 18);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Lion, the Witch and the Wardrobe', '9780590254762', 4.8, '1950-10-16', 19, 19);
+INSERT INTO Book (Title, Cover, Rating, RELEASE_DATE, Publisher_id, Author_id) VALUES ('The Raven', '9781553374732', 4.3, '1845-01-29', 20, 20);
 
-INSERT INTO Book (Title, Cover, Rating, Release_Date, Publisher_id, Author_id) VALUES ('Harry Potter and the Prisoner of Azkaban', 'https://m.media-amazon.com/images/I/812CcFkEPCL._AC_UF1000,1000_QL80_.jpg', 4.9, '1999-07-08', 3, 3);
-INSERT INTO Book (Title, Cover, Rating, Release_Date, Publisher_Id, Author_id) VALUES ('Harry Potter and the Chamber of Secrets', 'https://m.media-amazon.com/images/I/818umIdoruL._AC_UF1000,1000_QL80_.jpg', 4.8, '1998-07-02', 3, 3);
+INSERT INTO Book (Title, Cover, Rating, Release_Date, Publisher_id, Author_id) VALUES ('Harry Potter and the Prisoner of Azkaban', '9780439136358', 4.9, '1999-07-08', 3, 3);
+INSERT INTO Book (Title, Cover, Rating, Release_Date, Publisher_Id, Author_id) VALUES ('Harry Potter and the Chamber of Secrets', '9781551923703', 4.8, '1998-07-02', 3, 3);
+INSERT INTO Book (Title, Cover, Rating, Release_Date, Publisher_Id, Author_id) VALUES ('Alice`s Adventures in Wonderland', '9781846561238', 4.7, '1955-01-02', 21, 21);
+INSERT INTO Book (Title, Cover, Rating, Release_Date, Publisher_Id, Author_id) VALUES ('The Notebook', '9780751556896', 4.3, '1996-10-01', 22, 22);
+INSERT INTO Book (Title, Cover, Rating, Release_Date, Publisher_Id, Author_id) VALUES ('Jane Eyre', '9780753722701', 4.1, '1847-10-19', 1, 23);
 
 
 
 
 -- Insert data into BOOK_GENRES table
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (1, 1); -- 1984 is Dystopian
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (15, 1); -- 1984 is Classics
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (4, 1); -- 1984 is Sci-Fi
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (2, 2); -- Pride and Prejudice is Romance
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (8, 2); -- Pride and Prejudice is Historical Fiction
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (3, 3); -- Harry Potter is Fantasy
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (18, 3); -- Harry Potter is Childrens
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (9, 3); -- Harry Potter is Adventure
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (19, 3); -- Harry Potter is YA
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (9, 4); -- Tom Sawyer is Adventure
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (18, 4); -- Tom Sawyer is Childrens
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (15, 4); -- Tom Sawyer is Classics
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (8, 5); -- Gatsby is Historical Fiction
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (12, 6); -- Old Man and the Sea is Non-Fiction
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (15, 6); -- Old Man and the Sea is Classics
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (9, 6); -- Old Man and the Sea is Adventure
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (8, 7); -- Tale of Two Cities is Historical Fiction
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (8, 8); -- War and Peace is Historical Fiction
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (15, 8); -- War and Peace is Classics
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (9, 9); -- Moby-Dick is Adventure
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (4, 10); -- War of the Worlds is Science Fiction
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (4, 11); -- Foundation is Science Fiction
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (3, 12); -- Hobbit is Fantasy
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (19, 12); -- Hobbit is YA
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (9, 12); -- Hobbit is Adventure
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (5, 13); -- Orient Express is Mystery
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (5, 14); -- Hound of the Baskervilles is Mystery
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (1, 15); -- Brave New World is Dystopian
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (7, 16); -- Frankenstein is Horror
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (9, 17); -- 20000 Leagues is Adventure
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (3, 18); -- Game of Thrones is Fantasy
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (9, 18); -- Game of Thrones is Adventure
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (3, 19); -- Lion, Witch, and Wardrobe is Fantasy
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (7, 20); -- The Raven is Horror
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (3, 21); -- Harry Potter is Fantasy
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (18, 21); -- Harry Potter is Childrens
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (9, 21); -- Harry Potter is Adventure
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (19, 21); -- Harry Potter is YA
 INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (3, 22); -- Harry Potter is Fantasy
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (18, 22); -- Harry Potter is Childrens
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (9, 22); -- Harry Potter is Adventure
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (19, 22); -- Harry Potter is YA
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (3, 23); -- Alice in Wonderland is Fantasy
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (18, 23); -- Alice in Wonderland is Childrens
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (9, 23); -- Alice in Wonderland is Adventure
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (2, 24); -- The Notebook in Romance
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (2, 25); -- Jane Eyre in Romance
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (8, 25); -- Jane Eyre in Historical Fiction
+INSERT INTO BOOK_GENRES (Genres_id, Books_id) VALUES (15, 25); -- Jane Eyre in Classics
 
 -- Insert data into MY_USER table
 INSERT INTO MY_USER (FIRST_NAME, LAST_NAME, Username, Password, Email, iS_ADMIN, Goal) VALUES ('John', 'Doe', 'jdoe', 'password123', 'jdoe@example.com', 0, 10);
@@ -167,3 +203,16 @@ INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_
 INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (5, 6, 0, 1, NULL, NULL);
 INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (6, 7, 1, 0, 4, '2024-05-01');
 INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (7, 8, 1, 0, 5, '2023-06-01');
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (10, 21, 0, 1, NULL, NULL);
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (3, 22, 1, 0, 4, '2022-05-01');
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (11, 6, 1, 0, 5, '2023-06-01');
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (2, 17, 1, 0, 4, '2021-10-01');
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (15, 14, 1, 0, 5, '2023-09-01');
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (8, 22, 1, 0, 4, '2022-01-01');
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (19, 16, 1, 0, 3, '2024-06-01');
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (5, 4, 1, 0, 4, '2023-01-01');
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (12, 10, 1, 0, 5, '2023-09-01');
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (16, 23, 1, 0, 5, '2024-02-01');
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (2, 23, 1, 0, 4, '2023-04-01');
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (18, 25, 0, 1, NULL, NULL);
+INSERT INTO User_Book (USER_ID, Book_id, iS_READ, iS_WANT_TO_READ, Rating, dATE_READ) VALUES (20, 24, 0, 1, NULL, NULL);
